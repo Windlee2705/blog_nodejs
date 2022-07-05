e4cc27de0b0c9641f95f1a09c3a0b02d29522ba1
Phong Lê <phong270501@gmail.com>
2022-07-05 23:45:19 +0700

    add morgan to project

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const morgan = require('morgan')
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
-    var a =2, b=3;
-    const c=a+b+3;
+  
   res.send('Hello World!')
 })
 
